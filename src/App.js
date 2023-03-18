ae7b1077803bc5ae122c6c10150e1adc4f523c55
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-03-18 15:12:59 +0100

    css form added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import { useState } from "react";
 import Form from "./Form";
-import currencies from "./Currencies"
-
+import currencies from "./Currencies";
 
 function App() {
+  const [result, setResult] = useState();
 
-    const [result, setResult] = useState();
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({ name }) => name === currency).rate;
 
-    const calculateResult = (currency, amount) => {
-        const rate = currencies.find(({ name }) => name === currency).rate;
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
-        setResult({
-            targetAmount: amount / rate,
-            currency,
-        });
-    }
-
-    return ( <
-        >
-        <
-        Form result = { result }
-        calculateResult = { calculateResult }
-        /> < / >
-    );
-
-
-};
-
+  return (
+    <>
+      <Form result={result} calculateResult={calculateResult} />
+    </>
+  );
+}
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -3,74 +3,58 @@ import { useState } from "react";
 import currencies from "../Currencies";
 import Result from "../Result";
 
-
-
-
 const Form = ({ result, calculateResult }) => {
-    const [currency, setCurrency] = useState(currencies[0].name);
-    const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState(currencies[0].name);
+  const [amount, setAmount] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(currency, amount);
-    };
-    return ( <
-        form className = "form"
-        onSubmit = { onFormSubmit } >
-        <
-        fieldset className = "form__fieldset" >
-        <
-        p >
-        <
-        label >
-        <
-        span className = "form__labelText" > Amount in PLN * < /span> <
-        input value = { amount }
-        onChange = {
-            ({ target }) => setAmount(target.value)
-        }
-        className = "form__field"
-        type = "number"
-        min = "0.1"
-        step = "any"
-        name = "amount"
-        required / >
-        <
-        /label> < /
-        p > <
-        p >
-        <
-        label >
-        <
-        span className = "form__labelText" > I want to exchange
-        for: < /span> <
-        select value = { currency }
-        onChange = {
-            ({ target }) => setCurrency(target.value)
-        }
-        className = "form__field"
-        name = "currency" > {
-            currencies.map((currency => ( <
-                option key = { currency.name }
-                value = { currency.name } > { currency.name } <
-                /option>
-            )))
-        }
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
 
-        <
-        /select> < /
-        label > <
-        /p> <
-        p >
-        <
-        button className = "form__button" > { "calculate".toUpperCase() } < /button> < /
-        p > <
-        p className = "form__labelText form__labelText--result" > Result: < /p> <
-        Result result = { result }
-        /> < /
-        fieldset > <
-        /form>
-    );
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <p>
+          <label>
+            <span className="form__labelText"> Amount in PLN* </span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              className="form__field"
+              type="number"
+              min="0.1"
+              step="any"
+              name="amount"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">I want to exchange for:</span>
+            <select
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+              className="form__field"
+              name="currency"
+            >
+              {currencies.map((currency) => (
+                <option key={currency.name} value={currency.name}>
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
+        <p>
+          <button className="form__button">{"calculate".toUpperCase()}</button>
+        </p>
+        <p className="form__labelText form__labelText--result">Result:</p>
+        <Result result={result} />
+      </fieldset>
+    </form>
+  );
 };
 
 export default Form;
