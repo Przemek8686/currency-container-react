efd351ec6940ed9996df4d997191f1123b75714a
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-04-22 15:20:35 +0200

    Form styled components added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,16 @@
-import "./style.css";
 import { useState } from "react";
 import currencies from "../currencies";
 import Result from "../Result";
+import {
+  StyledForm,
+  Fieldset,
+  Label,
+  LabelText,
+  Input,
+  Select,
+  Button,
+  LabelTextResult,
+} from "./styled";
 
 const Form = ({ result, calculateResult }) => {
   const [currency, setCurrency] = useState(currencies[0].name);
@@ -13,47 +22,43 @@ const Form = ({ result, calculateResult }) => {
   };
 
   return (
-    <form className="form" onSubmit={onFormSubmit}>
-      <fieldset className="form__fieldset">
-        <p>
-          <label>
-            <span className="form__labelText"> Amount in PLN* </span>
-            <input
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              className="form__field"
-              type="number"
-              min="0.1"
-              step="any"
-              name="amount"
-              required
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <span className="form__labelText">I want to exchange for:</span>
-            <select
-              value={currency}
-              onChange={({ target }) => setCurrency(target.value)}
-              className="form__field"
-              name="currency"
-            >
-              {currencies.map((currency) => (
-                <option key={currency.name} value={currency.name}>
-                  {currency.name}
-                </option>
-              ))}
-            </select>
-          </label>
-        </p>
-        <p>
-          <button className="form__button">{"calculate".toUpperCase()}</button>
-        </p>
-        <p className="form__labelText form__labelText--result">Result:</p>
+    <StyledForm onSubmit={onFormSubmit}>
+      <Fieldset>
+        <Label>
+          <LabelText> Amount in PLN* </LabelText>
+          <Input
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            className="form__field"
+            type="number"
+            min="0.1"
+            step="any"
+            name="amount"
+            required
+          />
+        </Label>
+        <Label>
+          <LabelText>I want to exchange for:</LabelText>
+          <Select
+            value={currency}
+            onChange={({ target }) => setCurrency(target.value)}
+            className="form__field"
+            name="currency"
+          >
+            {currencies.map((currency) => (
+              <option key={currency.name} value={currency.name}>
+                {currency.name}
+              </option>
+            ))}
+          </Select>
+        </Label>
+        <Label>
+          <Button>{"calculate".toUpperCase()}</Button>
+        </Label>
+        <LabelTextResult>Result:</LabelTextResult>
         <Result result={result} />
-      </fieldset>
-    </form>
+      </Fieldset>
+    </StyledForm>
   );
 };
 
